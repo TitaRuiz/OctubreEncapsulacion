4784ec6a38c9b23b32e965b8450cec5806333a38
TitaRuiz <ggp2002es@gmail.com>
2023-10-27 15:12:39 +0200

    Initial commit

---
+++ src/com/corenetworks/presentacion/ProbarProducto.java
@@ -0,0 +1,11 @@
+package com.corenetworks.modelo;
+
+import com.corenetworks.modelo.Producto;
+
+public class ProbarProducto {
+
+    public static void main(String[] args) {
+        Producto p1 = new Producto();
+        p1.idProducto = 1;
+    }
+}
